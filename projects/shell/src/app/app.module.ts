8d094a9a78dabb3012af28711129a34df537e3dc
Yingying Chen <dinnerparty718@gmail.com>
2021-08-22 20:00:23 -0600

    before config

--- projects/shell/src/app/app.module.ts
+++ projects/shell/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PluginProxyComponent } from './plugins/plugin-proxy.component';
+import { HomeComponent } from './home/home.component';
+import { ConfigComponent } from './config/config.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PluginProxyComponent,
+    HomeComponent,
+    ConfigComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/shell/src/app/plugins/plugin-proxy.component.ts
@@ -0,0 +1,36 @@
+import { PluginOptions } from './plugin';
+import { Component, ComponentFactoryResolver, Injector, Input, OnChanges, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+
+@Component({
+  selector: 'app-plugin-proxy',
+  template: `
+    <ng-container #placeHolder></ng-container>
+  `
+})
+export class PluginProxyComponent implements OnInit, OnChanges {
+
+  @ViewChild('placeHolder', { read: ViewContainerRef, static: true })
+  viewContainer: ViewContainerRef;
+
+  @Input() options: PluginOptions;
+
+
+  constructor(private injector: Injector, private componentFactoryResolver: ComponentFactoryResolver) { }
+
+  ngOnInit(): void {
+  }
+
+  async ngOnChanges() {
+    this.viewContainer.clear();
+    const component = await loadRemoteModule(this.options)
+      .then(m => m[this.options.componentName]);
+
+    const factory = this.componentFactoryResolver.resolveComponentFactory(component);
+
+    this.viewContainer.createComponent(factory, undefined, this.injector);
+
+  }
+
+
+}
